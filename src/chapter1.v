5df99ae9a58787d912f2ba43ad15c6cf3d68fe96
Your Name <you@example.com>
2026-07-07 00:26:04 -0500

    sync

--- src/chapter1.v
+++ src/chapter1.v
@@ -1,3 +1,13 @@
 Require Import Stdlib.Vectors.Vector.
+From Stdlib Require Import Init.Prelude.
 
 Check Vector.t.
+
+
+Fixpoint append
+  {A : Type} {n m : nat}
+  (a : t A n) (b : t A m) : t A (n + m) :=
+  match b with
+  | (Vector.nil _) => a
+  | (Vector.cons _ b_elem m b) => _
+  end.
