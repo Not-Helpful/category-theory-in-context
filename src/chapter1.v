53f6db5dbae92b98a5d269d72e55683b4c2d3402
Your Name <you@example.com>
2026-07-07 23:53:36 -0500

    sync

--- src/chapter1.v
+++ src/chapter1.v
@@ -1,13 +1,19 @@
 Require Import Stdlib.Vectors.Vector.
 From Stdlib Require Import Init.Prelude.
+Require Import Stdlib.Numbers.NatInt.NZAdd.
+Require Import Arith.
 
 Check Vector.t.
-
 
 Fixpoint append
   {A : Type} {n m : nat}
   (a : t A n) (b : t A m) : t A (n + m) :=
-  match b with
-  | (Vector.nil _) => a
-  | (Vector.cons _ b_elem m b) => _
+  match b in t _ m0 return (t A (n+m0))
+  with
+  | (Vector.nil _) =>
+      match Nat.add_0_r n with
+      | eq_refl => a
+      end
+  | (Vector.cons _ b_elem m0 b') => 
+      Vector.cons A b_elem (n + S m0) (append a b')
   end.
