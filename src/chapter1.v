d7663fa34461336656ed2a6e543bc8c069daf98c
Your Name <you@example.com>
2026-07-09 01:05:56 -0500

    sync

--- src/chapter1.v
+++ src/chapter1.v
@@ -7,13 +7,5 @@ Check Vector.t.
 
 Fixpoint append
   {A : Type} {n m : nat}
-  (a : t A n) (b : t A m) : t A (n + m) :=
-  match b in t _ m0 return (t A (n+m0))
-  with
-  | (Vector.nil _) =>
-      match Nat.add_0_r n with
-      | eq_refl => a
-      end
-  | (Vector.cons _ b_elem m0 b') => 
-      Vector.cons A b_elem (n + S m0) (append a b')
-  end.
+  (a : t A n) (b : t A m) : t A (n + m).
+Proof.
